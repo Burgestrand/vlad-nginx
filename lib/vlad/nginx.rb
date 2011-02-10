b65996035e0c58736200b9bac92b2f6181fdc21e
Matthew Smith <matthewcalebsmith@gmail.com>
2011-02-10 12:53:59 -0800

    added with with_sudo method and nginx_use_sudo var

--- lib/vlad/nginx.rb
+++ lib/vlad/nginx.rb
@@ -8,31 +8,40 @@ require 'vlad'
 
 namespace :vlad do
   set :web_command, "nginx"
-  
+  set :nginx_use_sudo, false
+ 
+  def with_sudo(cmd)
+    if nginx_use_sudo
+      sudo cmd
+    else
+      run cmd
+    end
+  end
+
   namespace :web do
     desc "Gracefully stop the web servers."
     remote_task :quit, :roles => :web do
-      run "#{web_command} -s quit"
+      with_sudo "#{web_command} -s quit"
     end
     
     desc "Instantly kill the web servers."
     remote_task :kill, :roles => :web do
-      run "#{web_command} -s stop"
+      with_sudo "#{web_command} -s stop"
     end
     
     desc "Reload web server configuration."
     remote_task :reload, :roles => :web do
-      run "#{web_command} -s reload"
+      with_sudo "#{web_command} -s reload"
     end
     
     desc "Reopen web server logs."
     remote_task :reopen, :roles => :web do
-      run "#{web_command} -s reopen"
+      with_sudo "#{web_command} -s reopen"
     end
     
     desc "Start the web servers."
     remote_task :start, :roles => :web do
-      run web_command
+      with_sudo web_command
     end
   end
 end
